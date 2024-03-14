6441e0dd72e0b3016e156be1365ccc45b45b2a1d
CoderXKrishna <144244911+CoderXKrishna@users.noreply.github.com>
2024-03-13 23:17:19 -0700

    improve

--- helper_funcs/help_uploadbot.py
+++ helper_funcs/help_uploadbot.py
@@ -2,48 +2,43 @@
 # -*- coding: utf-8 -*-
 # (c) Shrimadhav U K
 
+import os
+import requests
+from time import time
+from humanize import intcomma, nbytes
+
 # the logging things
 import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-import os
-import requests
-
 def DetectFileSize(url):
-    r = requests.get(url, allow_redirects=True, stream=True)
+    r = requests.head(url, allow_redirects=True)
     total_size = int(r.headers.get("content-length", 0))
     return total_size
 
 
-def DownLoadFile(url, file_name, chunk_size, client, ud_type, message_id, chat_id):
+def DownLoadFile(url, file_name, chunk_size, client=None, ud_type=None, message_id=None, chat_id=None):
     if os.path.exists(file_name):
         os.remove(file_name)
     if not url:
         return file_name
     r = requests.get(url, allow_redirects=True, stream=True)
-    # https://stackoverflow.com/a/47342052/4723940
     total_size = int(r.headers.get("content-length", 0))
     downloaded_size = 0
     with open(file_name, 'wb') as fd:
+        start_time = time()
         for chunk in r.iter_content(chunk_size=chunk_size):
             if chunk:
                 fd.write(chunk)
                 downloaded_size += chunk_size
-            if client is not None:
-                if ((total_size // downloaded_size) % 5) == 0:
-                    time.sleep(0.3)
-                    try:
-                        client.edit_message_text(
-                            chat_id,
-                            message_id,
-                            text="{}: {} of {}".format(
-                                ud_type,
-                                humanbytes(downloaded_size),
-                                humanbytes(total_size)
-                            )
-                        )
-                    except:
-                        pass
+                elapsed_time = time() - start_time
+                if elapsed_time >= 0.3 and client is not None:
+                    start_time = time()
+                    client.edit_message_text(
+                        chat_id,
+                        message_id,
+                        text=f"{ud_type}: {intcomma(downloaded_size)} of {nbytes(total_size)}"
+                    )
     return file_name
