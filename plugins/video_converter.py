f972c323be11a26e6cb11187b7d0f9a2f194d0f2
CoderXKrishna <144244911+CoderXKrishna@users.noreply.github.com>
2024-03-13 23:17:46 -0700

    improve

--- plugins/video_converter.py
+++ plugins/video_converter.py
@@ -1,13 +1,19 @@
-
-# the logging things
 import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-
 import os
 import random
 import time
+from typing import Union
+
+import pyrogram
+from pyrogram.errors import UserNotParticipant, UserBannedInChannel
+
+import hachoir.metadata
+import hachoir.parser
+from PIL import Image
+
+# the logging things
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 # the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
@@ -18,55 +24,43 @@ else:
 # the Strings used for this "thing"
 from translation import Translation
 
-import pyrogram
+# Initialize Pyrogram Client
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
+app = pyrogram.Client(name="Mai_bOTs")
 
-#from helper_funcs.chat_base import TRChatBase
+# from helper_funcs.chat_base import TRChatBase
 from helper_funcs.display_progress import progress_for_pyrogram
 from helper_funcs.help_Nekmo_ffmpeg import take_screen_shot
 
-from pyrogram.types import InlineKeyboardButton
-from pyrogram.types import InlineKeyboardMarkup
-from pyrogram.errors import UserNotParticipant, UserBannedInChannel 
-from pyrogram import Client as Mai_bOTs 
-
-from hachoir.metadata import extractMetadata
-from hachoir.parser import createParser
-# https://stackoverflow.com/a/37631799/4723940
-from PIL import Image
-
-
-@Mai_bOTs.on_message(pyrogram.filters.command(["c2v"]))
-async def convert_to_video(bot, update):
+@app.on_message(pyrogram.filters.command(["c2v"]))
+async def convert_to_video(client: pyrogram.Client, message: pyrogram.types.Message):
     update_channel = Config.UPDATE_CHANNEL
     if update_channel:
         try:
-            user = await bot.get_chat_member(update_channel, update.chat.id)
+            user = await client.get_chat_member(update_channel, message.chat.id)
             if user.status == "kicked":
-               await update.reply_text(" Sorry, You are **B A N N E D**")
-               return
+                await message.reply_text("Sorry, You are **B A N N E D**")
+                return
         except UserNotParticipant:
-            #await update.reply_text(f"Join @{update_channel} To Use Me")
-            await update.reply_text(
+            await message.reply_text(
                 text="**Please Join My Update Channel Before Using Me..**",
                 reply_markup=InlineKeyboardMarkup([
-                    [ InlineKeyboardButton(text="Join My Updates Channel", url=f"https://t.me/{update_channel}")]
-              ])
+                    [InlineKeyboardButton(text="Join My Updates Channel", url=f"https://t.me/{update_channel}")]
+                ])
             )
-            return  
-    #TRChatBase(update.from_user.id, update.text, "c2v")
-    if update.reply_to_message is not None:
-        
+            return
+
+    if message.reply_to_message is not None:
         download_location = Config.DOWNLOAD_LOCATION + "/"
-        file_name=download_location
-        a = await bot.send_message(
-            chat_id=update.chat.id,
+        file_name = download_location
+        a = await client.send_message(
+            chat_id=message.chat.id,
             text=Translation.DOWNLOAD_START,
-            reply_to_message_id=update.message_id
+            reply_to_message_id=message.message_id
         )
         c_time = time.time()
-        the_real_download_location = await bot.download_media(
-            message=update.reply_to_message,
+        the_real_download_location = await client.download_media(
+            message=message.reply_to_message,
             file_name=download_location,
             progress=progress_for_pyrogram,
             progress_args=(
@@ -76,27 +70,20 @@ async def convert_to_video(bot, update):
             )
         )
         if the_real_download_location is not None:
-            await bot.edit_message_text(
+            await client.edit_message_text(
                 text=Translation.SAVED_RECVD_DOC_FILE,
-                chat_id=update.chat.id,
+                chat_id=message.chat.id,
                 message_id=a.message_id
             )
-            # don't care about the extension
-           # await bot.edit_message_text(
-              #  text=Translation.UPLOAD_START,
-             #   chat_id=update.chat.id,
-            #    message_id=a.message_id
-          #  )
             logger.info(the_real_download_location)
-            # get the correct width, height, and duration for videos greater than 10MB
-            # ref: message from @BotSupport
+
             width = 0
             height = 0
             duration = 0
-            metadata = extractMetadata(createParser(the_real_download_location))
+            metadata = hachoir.metadata.extractMetadata(hachoir.parser.createParser(the_real_download_location))
             if metadata.has("duration"):
                 duration = metadata.get('duration').seconds
-            thumb_image_path = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
+            thumb_image_path = Config.DOWNLOAD_LOCATION + "/" + str(message.from_user.id) + ".jpg"
             if not os.path.exists(thumb_image_path):
                 thumb_image_path = await take_screen_shot(
                     the_real_download_location,
@@ -107,56 +94,45 @@ async def convert_to_video(bot, update):
                     )
                 )
             logger.info(thumb_image_path)
-            # 'thumb_image_path' will be available now
-            metadata = extractMetadata(createParser(thumb_image_path))
+
+            metadata = hachoir.metadata.extractMetadata(hachoir.parser.createParser(thumb_image_path))
             if metadata.has("width"):
                 width = metadata.get("width")
             if metadata.has("height"):
                 height = metadata.get("height")
-            # get the correct width, height, and duration for videos greater than 10MB
-            # resize image
-            # ref: https://t.me/PyrogramChat/44663
-            # https://stackoverflow.com/a/21669827/4723940
+
             Image.open(thumb_image_path).convert("RGB").save(thumb_image_path)
             img = Image.open(thumb_image_path)
-            # https://stackoverflow.com/a/37631799/4723940
-            # img.thumbnail((90, 90))
             img.resize((90, height))
             img.save(thumb_image_path, "JPEG")
-            # https://pillow.readthedocs.io/en/3.1.x/reference/Image.html#create-thumbnails
-            # try to upload file
+
             c_time = time.time()
-            await bot.send_video(
-                chat_id=update.chat.id,
-                video=the_real_download_location,
-                duration=duration,
-                width=width,
-                height=height,
-                supports_streaming=True,
-                # reply_markup=reply_markup,
-                thumb=thumb_image_path,
-                reply_to_message_id=update.reply_to_message.message_id,
-                progress=progress_for_pyrogram,
-                progress_args=(
-                    Translation.UPLOAD_START,
-                    a,
-                    c_time
-                )
-            )
             try:
-                os.remove(the_real_download_location)
-              #  os.remove(thumb_image_path)
-            except:
-                pass
-            await bot.edit_message_text(
-                text=Translation.AFTER_SUCCESSFUL_UPLOAD_MSG,
-                chat_id=update.chat.id,
-                message_id=a.message_id,
-                disable_web_page_preview=True
-            )
-    else:
-        await bot.send_message(
-            chat_id=update.chat.id,
-            text=Translation.REPLY_TO_FILE_FOR_CONVERT,
-            reply_to_message_id=update.message_id
-        )
+                await client.send_video(
+                    chat_id=message.chat.id,
+                    video=the_real_download_location,
+                    duration=duration,
+                    width=width,
+                    height=height,
+                    supports_streaming=True,
+                    thumb=thumb_image_path,
+                    reply_to_message_id=message.reply_to_message.message_id,
+                    progress=progress_for_pyrogram,
+                    progress_args=(
+                        Translation.UPLOAD_START,
+                        a,
+                        c_time
+                    )
+                )
+            except UserBannedInChannel:
+                await message.reply_text(
+                    text="**You are banned from this channel.**",
+                    reply_markup=InlineKeyboardMarkup([
+                        [InlineKeyboardButton(text="Join My Updates Channel", url=f"https://t.me/{update_channel}")]
+                    ])
+                )
+            finally:
+                try:
+                    os.remove(the_real_download_location)
+                    os.remove(thumb_image_path)
+                except
